ec2faf102f0056e9c678dc62103142c4af736ef3
metherton <martinetherton63@gmail.com>
2023-08-02 21:31:56 +0200

    back to basics

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 <?php
 require __DIR__ . '/twilio-php-main/src/Twilio/autoload.php';
 echo "My first actual PHP script!";
+
+$sid = "ACXXXXXX";
+$token = "YYYYYY";
+$client = new Twilio\Rest\Client($sid, $token);
+echo $client;
+
 ?>
 
 </body>
