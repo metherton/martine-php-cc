3205a6557b7bab57fe254d8bb0828bd4fe378a75
metherton <martinetherton63@gmail.com>
2023-08-02 23:45:59 +0200

    fix error

--- index.php
+++ index.php
@@ -10,16 +10,13 @@ echo "My first actual PHP script!";
 $sid = getenv('ACCOUNT_SID');
 $auth_token = getenv('TOKEN');
 echo "create client";
-$client = new Twilio\Rest\Client($sid, $token);
-echo $client;
-
 // A Twilio number you own with Voice capabilities
 $twilio_number = "+3242776825";
-
 // Where to make a voice call (your cell phone?)
 $to_number = "+31624543741";
 
 $client = new Client($account_sid, $auth_token);
+echo $client;
 $client->account->calls->create(
     $to_number,
     $twilio_number,
