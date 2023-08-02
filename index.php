f1a6e22aa96b013f2c1953578c4840b4cc5293a5
metherton <martinetherton63@gmail.com>
2023-08-02 23:13:02 +0200

    use twilio

--- index.php
+++ index.php
@@ -7,10 +7,26 @@ require __DIR__ . '/twilio-php-main/src/Twilio/autoload.php';
 echo "My first actual PHP script!";
 
 $sid = getenv('ACCOUNT_SID');
+$auth_token = getenv('TOKEN');
 
-$token = "YYYYYY";
 $client = new Twilio\Rest\Client($sid, $token);
 echo $client;
+
+// A Twilio number you own with Voice capabilities
+$twilio_number = "+3242776825";
+
+// Where to make a voice call (your cell phone?)
+$to_number = "+31624543741";
+
+$client = new Client($account_sid, $auth_token);
+$client->account->calls->create(
+    $to_number,
+    $twilio_number,
+    array(
+        "url" => "http://demo.twilio.com/docs/voice.xml"
+    )
+);
+
 
 ?>
 
